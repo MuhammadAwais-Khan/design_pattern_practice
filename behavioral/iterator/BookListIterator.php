78bf8366bdab1b05ae8224351cc801500c293314
MuhammadAwais-Khan <awaiskhan.se@gmail.com>
2021-03-26 17:50:12 +0500

    iterator

--- behavioral/iterator/BookListIterator.php
+++ behavioral/iterator/BookListIterator.php
@@ -1,7 +1,30 @@
 <?php
 
+class BookListIterator {
+    protected $bookList;
+    protected $currentBook = 0;
 
-class BookListIterator
-{
-
+    public function __construct(BookList $bookList_in) {
+        $this->bookList = $bookList_in;
+    }
+    public function getCurrentBook() {
+        if (($this->currentBook > 0) &&
+            ($this->bookList->getBookCount() >= $this->currentBook)) {
+            return $this->bookList->getBook($this->currentBook);
+        }
+    }
+    public function getNextBook() {
+        if ($this->hasNextBook()) {
+            return $this->bookList->getBook(++$this->currentBook);
+        } else {
+            return NULL;
+        }
+    }
+    public function hasNextBook() {
+        if ($this->bookList->getBookCount() > $this->currentBook) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
 }
--- behavioral/iterator/workspace/iterator.php
+++ behavioral/iterator/workspace/iterator.php
@@ -1,1 +1,56 @@
 <?php
+
+require_once ('../Book.php');
+require_once ('../BookList.php');
+require_once ('../BookListIterator.php');
+require_once ('../BookListReverseIterator.php');
+
+writeln('BEGIN TESTING ITERATOR PATTERN');
+writeln('');
+
+$firstBook = new Book('Core PHP Programming, Third Edition', 'Atkinson and Suraski');
+$secondBook = new Book('PHP Bible', 'Converse and Park');
+$thirdBook = new Book('Design Patterns', 'Gamma, Helm, Johnson, and Vlissides');
+
+$books = new BookList();
+$books->addBook($firstBook);
+$books->addBook($secondBook);
+$books->addBook($thirdBook);
+
+writeln('Testing the Iterator');
+
+$booksIterator = new BookListIterator($books);
+
+while ($booksIterator->hasNextBook()) {
+    $book = $booksIterator->getNextBook();
+    writeln('getting next book with iterator :');
+    writeln($book->getAuthorAndTitle());
+    writeln('');
+}
+
+$book = $booksIterator->getCurrentBook();
+writeln('getting current book with iterator :');
+writeln($book->getAuthorAndTitle());
+writeln('');
+
+writeln('Testing the Reverse Iterator');
+
+$booksReverseIterator = new BookListReverseIterator($books);
+
+while ($booksReverseIterator->hasNextBook()) {
+    $book = $booksReverseIterator->getNextBook();
+    writeln('getting next book with reverse iterator :');
+    writeln($book->getAuthorAndTitle());
+    writeln('');
+}
+
+$book = $booksReverseIterator->getCurrentBook();
+writeln('getting current book with reverse iterator :');
+writeln($book->getAuthorAndTitle());
+writeln('');
+
+writeln('END TESTING ITERATOR PATTERN');
+
+function writeln($line_in) {
+    echo $line_in."<br/>";
+}
