dbf03e9535bd4294f939b8318e5aa303cff6c71f
MuhammadAwais-Khan <awaiskhan.se@gmail.com>
2021-03-29 10:31:58 +0500

    mediator

--- behavioral/mediator/BookColleague.php
+++ behavioral/mediator/BookColleague.php
@@ -1,7 +1,12 @@
 <?php
 
-
-class BookColleague
-{
-
+abstract class BookColleague {
+    private $mediator;
+    function __construct($mediator_in) {
+        $this->mediator = $mediator_in;
+    }
+    function getMediator() {return $this->mediator;}
+    function changed($changingClassIn) {
+        getMediator()->titleChanged($changingClassIn);
+    }
 }
--- behavioral/mediator/workspace/mediator.php
+++ behavioral/mediator/workspace/mediator.php
@@ -1,1 +1,39 @@
 <?php
+
+require_once ('../BookMediator.php');
+require_once ('../BookColleague.php');
+require_once ('../BookAuthorColleague.php');
+require_once ('../BookTitleColleague.php');
+
+writeln('BEGIN TESTING MEDIATOR PATTERN');
+writeln('');
+
+$mediator = new BookMediator('Gamma, Helm, Johnson, and Vlissides', 'Design Patterns');
+
+$author = $mediator->getAuthor();
+$title = $mediator->getTitle();
+
+writeln('Original Author and Title: ');
+writeln('author: ' . $author->getAuthor());
+writeln('title: ' . $title->getTitle());
+writeln('');
+
+$author->setAuthorLowerCase();
+
+writeln('After Author set to Lower Case: ');
+writeln('author: ' . $author->getAuthor());
+writeln('title: ' . $title->getTitle());
+writeln('');
+
+$title->setTitleUpperCase();
+
+writeln('After Title set to Upper Case: ');
+writeln('author: ' . $author->getAuthor());
+writeln('title: ' . $title->getTitle());
+writeln('');
+
+writeln('END TESTING MEDIATOR PATTERN');
+
+function writeln($line_in) {
+    echo $line_in.'<br/>';
+}
